7ba4e165fe547ac6a500dd0392b407975317e623
tylteng <teng.ty4@gmail.com>
2017-12-18 15:37:22 -0500

    completed exercise 1

---
+++ exercise1.rb
@@ -0,0 +1,9 @@
+require 'httparty'
+
+toronto_wards_response = HTTParty.get('https://represent.opennorth.ca/boundaries/toronto-wards/')
+
+toronto_wards_json = JSON.parse(toronto_wards_response.body)
+
+toronto_wards_json["objects"].each do |x|
+  p x["name"]
+end
